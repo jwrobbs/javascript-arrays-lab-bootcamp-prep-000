16256d5fb43d0d5644d53f65c09bc8b2afb2d0aa
Josh Robbs <josh@rustbeltwebworks.com>
2018-05-15 01:24:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function prependKitten(name) {
   return newArray
 }
 function removeLastKitten() {
-  var newArray = kittens.slice(1, kittens.length - 1)
+  var newArray = kittens.slice(0, kittens.length - 1)
   return newArray
 }
