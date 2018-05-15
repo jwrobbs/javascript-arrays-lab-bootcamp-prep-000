2d802545b8bdab00d30c6c9b8f3af5d792b68e2e
Josh Robbs <josh@rustbeltwebworks.com>
2018-05-15 01:24:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+function appendKitten(name) {
+  var newArray = [...kittens, name]
+  return newArray
+}
+function prependKitten(name) {
+  var newArray = [name,...kittens]
+  return newArray
+}
+function removeLastKitten() {
+  var newArray = kittens.slice(1, kittens.length - 1)
+  return newArray
+}
